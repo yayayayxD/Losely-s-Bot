e8b414bd78218a44f51718e54995b0186516dd40
yayayayxD <47941072+yayayayxD@users.noreply.github.com>
2019-03-06 14:48:49 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,46 @@
+const botconfig = require("./botconfig.json");
+const tokenfile = require("./token.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({ disableEveryone: true });
+const ms = require("ms");
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, file) => {
+
+    if (err) console.log(err);
+
+    let jsfile = file.filter(f => f.split(".").pop() === "js")
+    if (jsfile.length <= 0) {
+        console.log("Couldn't find command!");
+        return;
+    }
+
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
+
+});
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is online!`);
+    bot.user.setActivity("Hent... ", {type: "WATCHING"});
+});
+
+bot.on("message", async message => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if (commandfile) commandfile.run(bot, message, args);
+
+});
+
+bot.login(tokenfile.token);
